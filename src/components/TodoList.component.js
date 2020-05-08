9414877d07aaf785db193e0e146c2acd5543b9c2
p.wysocki <p.wysocki@infinity-group.pl>
2020-05-08 14:46:36 +0200

    todo aktywne/nieaktywne - dwie listy

--- src/components/TodoList.component.js
+++ src/components/TodoList.component.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 
-function TodoListComponent({ todoList, actionMarkTodo }) {
-  const renderTodos = () =>
+function TodoListComponent({ todoList, todoListCompleted, actionMarkTodo }) {
+  const renderTodos = (todoList) =>
     todoList.map((todoElement) => (
       <div key={todoElement.id} onClick={() => actionMarkTodo(todoElement.id)}>
         {todoElement.name}
@@ -10,15 +10,18 @@ function TodoListComponent({ todoList, actionMarkTodo }) {
     ));
   return (
     <div className="App">
-      <h2>Lista Todo:</h2>
-      {renderTodos()}
+      <h2>Lista Todo Aktywnych:</h2>
+      {renderTodos(todoList)}
+      <h2>Lista Todo Wykonanych:</h2>
+      {renderTodos(todoListCompleted)}
     </div>
   );
 }
 
 const mapStateToProps = (state) => {
   return {
-    todoList: state.todo,
+    todoList: state.todo.filter((element) => !element.completed),
+    todoListCompleted: state.todo.filter((element) => element.completed),
   };
 };
 
--- src/index.js
+++ src/index.js
@@ -49,8 +49,16 @@ const reducer = (state = INITIAL_STATE, action) => {
         ],
       });
     case "MARK_TODO":
-      console.log(action.TodoId);
-      return state;
+      const newTodo = [...state.todo];
+      newTodo.map((element) =>
+        element.id === action.todoId
+          ? (element.completed = !element.completed)
+          : null
+      );
+      return (state = {
+        ...state,
+        todo: [...newTodo],
+      });
     default:
       return state;
   }
